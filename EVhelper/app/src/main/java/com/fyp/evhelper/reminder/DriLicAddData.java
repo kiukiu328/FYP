f9b2603f0c09f96b250e6b21a9c0dc7edbac913f
smoovlattetester <88876295+smoovlattetester@users.noreply.github.com>
2022-04-25 11:11:11 +0800

    Update DriLicAddData.java

--- EVhelper/app/src/main/java/com/fyp/evhelper/reminder/DriLicAddData.java
+++ EVhelper/app/src/main/java/com/fyp/evhelper/reminder/DriLicAddData.java
@@ -40,7 +40,7 @@ public class DriLicAddData extends AppCompatActivity {
     TextView textview_data, textDriSample2;
     Bitmap bitmap;
     String driLicNo, driName, driLicDate, dataNo;
-    ImageView imageDriSample;
+    ImageView imageDriSample,imageCapture;
     AlertDialog.Builder confirm;
     private static final int REQUEST_CAMERA_CODE = 100;
     Intent intent;
@@ -48,6 +48,11 @@ public class DriLicAddData extends AppCompatActivity {
     SQLiteDatabase db= null;
     String sql;
     long result = 0;
+
+    //19-4-2022
+    LinearLayout dri_showDate;
+    EditText edit_driName,edit_driLicNo,edit_driLicDate;
+    //19-4-2022
 
     //search->
     //crop image                        --> a,a
@@ -62,10 +67,14 @@ public class DriLicAddData extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_dri_lic_add_data);
 
         textDriSample2 = findViewById(R.id.textDriSample2);
         imageDriSample = findViewById(R.id.imageDriSample);
+        imageCapture =findViewById(R.id.imageCapture);
         button_back = findViewById(R.id.button_back);
         button_capture = findViewById(R.id.button_capture);
         button_saveDriLicData = findViewById(R.id.button_saveDriLicData);
@@ -74,6 +83,31 @@ public class DriLicAddData extends AppCompatActivity {
 
         final MediaPlayer correct_sound = MediaPlayer.create(this,R.raw.ding_sound);
         final MediaPlayer click_sound = MediaPlayer.create(this,R.raw.click_sound);
+
+        //19-04-2022
+        dri_showDate = (LinearLayout) findViewById(R.id.dri_showDate);
+        edit_driLicNo = findViewById(R.id.edit_driLicNo);
+        edit_driName = findViewById(R.id.edit_driName);
+        edit_driLicDate = findViewById(R.id.edit_driLicDate);;
+        edit_driLicDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] result = edit_driLicDate.getText().toString().split("/");
+                final int year = Integer.parseInt(result[2]);
+                final int month = Integer.parseInt(result[1]);
+                final int day = Integer.parseInt(result[0]);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(DriLicAddData.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        edit_driLicDate.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+        //19-04-2022
 
         // camera permission \\
         if(ContextCompat.checkSelfPermission(DriLicAddData.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -110,39 +144,43 @@ public class DriLicAddData extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 click_sound.start();
-                try {
-                    confirm.setTitle("Confirm")
-                            .setMessage("Make sure the Driving Licence Number is Correct!"+"\n")
-                            .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // save the data to firebase \\
-                                    DriLicData driLicData = new DriLicData(driLicNo, driName, driLicDate);
-                                    reference.child(driLicNo).setValue(driLicData);
-                                    //e,end\\
+                //try {
+                //confirm.setTitle("Confirm")
+                //.setMessage("Make sure the Driving Licence Number is Correct!"+"\n")
+                //.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                //@Override
+                //public void onClick(DialogInterface dialog, int which) {
+                // save the data to firebase \\
+                if(edit_driLicDate.getText().toString().length() !=10){
+                    Toast.makeText(DriLicAddData.this, "The data is incorrect. Please correct the data or provide the vehicle license again", Toast.LENGTH_LONG).show();
+                }else{
+                    DriLicData driLicData = new DriLicData(edit_driLicNo.getText().toString(),edit_driName.getText().toString(),edit_driLicDate.getText().toString());
+                    reference.child(driLicNo).setValue(driLicData);
+                    //e,end\\
 
-                                    // save the data to db \\
-                                    db= SQLiteDatabase.openDatabase("/data/data/com.example.v12_1/LicenceDB", null ,SQLiteDatabase.CREATE_IF_NECESSARY);
-                                    sql = "CREATE TABLE IF NOT EXISTS DrivingLicence(driLicDataNo INTEGER PRIMARY KEY AUTOINCREMENT, driLicNo text,driName text, driLicDate date);";
-                                    db.execSQL(sql);
-                                    String row = "INSERT INTO DrivingLicence(driLicNo,driName,driLicDate) Values ('"+driLicNo+"','"+driName+"','"+driLicDate+"')";
-                                    db.execSQL(row);
-                                    db.close();
-                                    //e,end\\
-                                    Toast.makeText(DriLicAddData.this, "Save Data Successfully", Toast.LENGTH_LONG).show();
-                                    correct_sound.start();
-                                    nextActivity();
-                                }
-                            }).setNegativeButton("Back", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    }).show();
-                }catch (Exception e){
-                    Toast.makeText(DriLicAddData.this,"Please Retake again!!\nThe data incorrect!!",Toast.LENGTH_SHORT).show();
+                    // save the data to db \\
+                    db= SQLiteDatabase.openDatabase("/data/data/com.fyp.evhelper.reminder/LicenceDB", null ,SQLiteDatabase.CREATE_IF_NECESSARY);
+                    sql = "CREATE TABLE IF NOT EXISTS DrivingLicence(driLicDataNo INTEGER PRIMARY KEY AUTOINCREMENT, driLicNo text,driName text, driLicDate date);";
+                    db.execSQL(sql);
+                    String row = "INSERT INTO DrivingLicence(driLicNo,driName,driLicDate) Values ('"+edit_driLicNo.getText().toString()+"','"+edit_driName.getText().toString()+"','"+edit_driLicDate.getText().toString()+"')";
+                    db.execSQL(row);
+                    db.close();
+                    //e,end\\
+                    Toast.makeText(DriLicAddData.this, "Save Data Successfully", Toast.LENGTH_LONG).show();
+                    correct_sound.start();
+                    nextActivity();
                 }
             }
+            //).setNegativeButton("Back", new DialogInterface.OnClickListener() {
+            //@Override
+            //public void onClick(DialogInterface dialog, int which) {
+            //dialog.cancel();
+            //}
+            //}).show();
+            //}catch (Exception e){
+            //Toast.makeText(DriLicAddData.this,"Please Retake again!!\nThe data incorrect!!",Toast.LENGTH_SHORT).show();
+            //}
+            //}
         });
         //b,b end\\
     }
@@ -156,6 +194,7 @@ public class DriLicAddData extends AppCompatActivity {
                 Uri resultUri = result.getUri();
                 try {
                     bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),resultUri);
+                    imageCapture.setImageBitmap(bitmap);
                     getTextFromImage(bitmap);
 
                 } catch (IOException e) {
@@ -204,20 +243,34 @@ public class DriLicAddData extends AppCompatActivity {
                     }
                     // check the valid date and print it\\
                     if(i>=3 && i<=7){
-                        if(textBlock.getValue().replace("Valid to","")
-                        .replaceAll("[A-Z]","").replace(" ","").trim().length() == 10){
+                        if(textBlock.getValue().replaceAll("[a-z]","").replaceAll("[A-Z]","").replace("Valid to","")
+                                .replace(" ","").trim().length() == 10){
                             driLicDate = textBlock.getValue();
                             stringBuilder.append(textBlock.getValue()+"\n");
                         }
                     }
                 }
                 setData(driLicNo,driName,driLicDate);
-                textview_data.setText(stringBuilder.toString());
+                //19-4-2022
+                edit_driLicNo.setText(driLicNo);
+                edit_driName.setText(driName);
+                edit_driLicDate.setText(driLicDate);
+                dri_showDate.setVisibility(View.VISIBLE);
+                imageCapture.setMaxWidth(imageCapture.getWidth() + imageCapture.getWidth());
+                imageCapture.setMaxHeight(imageCapture.getHeight() + imageCapture.getHeight());
+                //imageCapture.requestLayout();
+                imageCapture.setVisibility(View.VISIBLE);
+                //19-4-2022
+
+                //textview_data.setText(stringBuilder.toString());
                 button_capture.setText("Retake");
-                textDriSample2.setText("If the data is incorrect, you can be provided later or retake the information.");
+                textDriSample2.setVisibility(View.GONE);
+                //textDriSample2.setText("If the data is incorrect, you can be provided later or retake the information.");
                 imageDriSample.setVisibility(View.GONE);
-                button_back.setVisibility(View.GONE);
+                //button_back.setVisibility(View.GONE);
                 button_saveDriLicData.setVisibility(View.VISIBLE);
+
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
